4a29fcf66de4436a60d5b7617ebc3782bd9b19fd
Вадим Бабаев <0463503@gmail.com>
2023-03-05 22:11:15 +0100

    rewrite Card add delete comments

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,34 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import "./index.css"
 
-const Card = ({ productImg, title, price, id, onBuy, srcBuy }) => {
-  const onClickBuy = () => {
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  onClickBuy = () => {
+    const { productImg, title, price, id, onBuy } = this.props;
     onBuy({ productImg, title, price, id });
   };
-  return (
-    <div className="card">
-      <div className="favorite"></div>
-      <img src={productImg} alt="sneakers_photo" width={133} height={112} />
-      <p>{title}</p>
-      <div className="infoBtn">
-        <div className="info">
-          <span>
-            Price:
-            <br />
-            <b>{price}</b>
-          </span>
+
+  render() {
+    const { productImg, title, price, srcBuy } = this.props;
+    return (
+      <div className="card">
+        <div className="favorite"></div>
+        <img src={productImg} alt="sneakers_photo" width={133} height={112} />
+        <p>{title}</p>
+        <div className="infoBtn">
+          <div className="info">
+            <span>
+              Price:
+              <br />
+              <b>{price}</b>
+            </span>
+          </div>
+          <img
+            width={11}
+            height={11}
+            className="butn"
+            onClick={this.onClickBuy}
+            src={srcBuy}
+            alt="btn"
+          />
         </div>
-        <img
-          width={11}
-          height={11}
-          className="butn"
-          onClick={onClickBuy}
-          src={srcBuy}
-          alt="btn"
-        />
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Card;
